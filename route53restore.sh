b7beabdd87a681c19e9f24c39ee995cde4d88390
root <root@test.kubeclould.com>
2018-12-01 10:49:42 +0000

    added restore scripts

---
+++ route53restore.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TIME=`date +"%Y%m%d%H"`
+BACKUP_BUCKET=route53lambdabackup2018
+if [ $# -lt 2 ]
+then
+  echo "Usage: \$1 zonedata filename to restore Route53 \$2 zone id to restore data"
+else
+  ZONEFILE=$1
+  ZONEID=$2
+  aws s3 cp s3://$BACKUP_BUCKET/$ZONEFILE .
+  ZONEFILENAME="$ZONEID"_"$TIME".bkp
+  cli53 import --file $1 --replace --wait $2
+fi
